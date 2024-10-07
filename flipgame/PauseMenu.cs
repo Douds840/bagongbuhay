f0ac135efa26bd801e88ccc68967ef50e83d5400
Douds840 <dionelopatanpatan30@gmail.com>
2024-10-07 19:55:32 +0800

    Add files via upload

---
+++ flipgame/PauseMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject A;
+    [SerializeField] GameObject B;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        A.SetActive(false);
+        B.SetActive(false);
+
+    }
+    public void Home()
+    {
+        SceneManager.LoadScene("Easy");
+        Time.timeScale = 1;
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        A.SetActive(true);
+        B.SetActive(true);
+
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Time.timeScale=1;
+    }
+}
